8d70723f7aff7b0dcd793b42afecf9f00a6a5add
akash5656 <31134298+akash5656@users.noreply.github.com>
2025-07-20 13:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (21.29%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> PostorderTraversal(TreeNode root) {
+        List<int> res = new();
+        Traverse(root,res);
+        return res;
+    }
+
+    public void Traverse(TreeNode curr, List<int> result)
+    {
+        if(curr == null)
+            return;
+        
+        Traverse(curr.left,result);
+        Traverse(curr.right,result);
+        result.Add(curr.val);
+    }
+}
