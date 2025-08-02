cefc8c9036a98357f42671f4ed9cae5103d9524e
akash5656 <31134298+akash5656@users.noreply.github.com>
2025-08-02 23:19:15 +0530

    Time: 55 ms (26.29%) | Memory: 65.7 MB (82.40%) - LeetSync

---
+++ 15-3sum/3sum.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+
+        Array.Sort(nums);
+
+        List<IList<int>> ans = new();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(i == 0  || nums[i] != nums[i - 1])
+                CheckSum(nums,i, ans);
+        }
+
+        return ans;
+        
+    }
+
+    public void CheckSum(int[] nums,int idx,IList<IList<int>> ans)
+    {
+        int i = idx + 1;
+        int j = nums.Length - 1;
+
+        while(i < j)
+        {
+            int sum = nums[i] + nums[idx] + nums[j];
+            if(sum > 0)
+            {
+                --j;
+            }
+            else if(sum < 0)
+            {
+                ++i;
+            }
+            else
+            {
+                ans.Add(new List<int>(){nums[i],nums[j],nums[idx]});
+                ++i;
+                --j;
+                while (i < j && nums[i] == nums[i - 1]) ++i;
+            }
+        }
+    }
+}
