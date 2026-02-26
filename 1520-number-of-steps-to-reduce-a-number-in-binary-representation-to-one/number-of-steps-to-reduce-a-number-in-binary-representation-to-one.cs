509c05f0e71135083e4d067a1c8efa3da7a885e4
akash5656 <31134298+akash5656@users.noreply.github.com>
2026-02-26 22:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 38.7 MB (54.17%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int NumSteps(string s) {
+
+        int n = s.Length;
+
+        int carry = 0;
+        int op = 0;
+
+        for(int i=n-1;i>=1;i--)
+        {
+            if(((s[i]-'0')+carry)%2 == 1 )
+            {
+                op+=2;
+                carry = 1;
+            }
+            else
+            {
+                op+=1;
+            }
+        }
+
+        return op +  carry;
+        
+    }
+}
